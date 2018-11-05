2b39c3005716d0cdb8750cda5a495d226ad781c5
Jody Isaguirre <jodyisaguirre@Jodys-MacBook-Air.local>
2018-11-05 11:24:28 -0700

    commented out KNEX

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
 // Update with your config settings.
 
-module.exports = {
-
-  development: {
-    client: 'pg',
-    connection:'postgres://localhost/characters'
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true'
-  }
-
-};
+// module.exports = {
+//
+//   development: {
+//     client: 'pg',
+//     connection:'postgres://localhost/characters'
+//   },
+//
+//   production: {
+//     client: 'postgresql',
+//     connection: process.env.DATABASE_URL + '?ssl=true'
+//   }
+//
+// };
